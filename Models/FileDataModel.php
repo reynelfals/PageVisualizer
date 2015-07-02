b0b1cd7f845058138c6f284a4f6b9344c4eb69d2
zayfals2015 <zayfals2015@may21>
2015-07-02 01:01:14 -0500

    Doing data files relatives to the application

--- Models/FileDataModel.php
+++ Models/FileDataModel.php
@@ -22,7 +22,7 @@ class FileDataModel implements IDocDataModel {
 
     public function __construct() {
         $ini_array = parse_ini_file(INI_FILE);
-        $this->dir = $ini_array[FileDataModel::FILE_DIR_KEY];
+        $this->dir = dirname(__DIR__).DIRECTORY_SEPARATOR.$ini_array[FileDataModel::FILE_DIR_KEY];
     }
 
     public function getTitles() {
